8d6500583a115bbd07883378eb72194c87e93f9a
KateTurnerFSA <39065390+KateTurnerFSA@users.noreply.github.com>
2018-12-10 18:29:44 -0500

    Update solution_right_triangle.js

--- 03_right_triangle/solution_right_triangle.js
+++ 03_right_triangle/solution_right_triangle.js
@@ -1,17 +1,1 @@
-function rightTriangle (num) {
-  let triangle= '';
-  for (let i = 0; i < num; i++){
-    for (let j = 0; j < num; j++){
-      if ((i + j) < num) {
-        triangle = triangle + '*';
-      } else {
-        triangle = triangle + '';
-      }
-    } triangle = triangle + '\n';
-  }
-  return triangle.slice(0, triangle.length-1);
-}
 
-/* Nice job!! One note, you can eliminate the else statement in the body of your for loop. That else statement isn't doing anything, so it isn't necessary.
-It's totally fine to have an if() statement and no else :D
- */
